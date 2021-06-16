032e6d6f74a82f1fe78af863d7f97b63add29512
Ivan Savkov <ivansavkov1387@gmail.com>
2021-06-16 16:19:34 +0300

    Add something

--- UITextViewHW/ViewController.swift
+++ UITextViewHW/ViewController.swift
@@ -8,11 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    
+    
+    @IBOutlet weak var blackButton: UIButton!
+    @IBOutlet weak var whiteButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    @IBOutlet weak var reduseThickness: UIButton!
+    @IBOutlet weak var increaseThickness: UIButton!
+    
+    @IBOutlet weak var fontStylePicker: UIPickerView!
+    
+    @IBOutlet weak var shareButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fontSizeSlider.minimumValue = 1
+        fontSizeSlider.maximumValue = 50
     }
+    
+    func changeColor(button: UIButton) {
+        switch button {
+        case blackButton: textView.textColor = .black
+        case whiteButton: textView.textColor = .white
+        case redButton: textView.textColor = .red
+        case yellowButton: textView.textColor = .yellow
+        case blueButton: textView.textColor = .blue
+        default:
+            textView.textColor = .black
+        }
+    }
+    
+    func changeFontThickness(button: UIButton) {
+        switch button {
+        case increaseThickness:
+            textView.text = textView.text.uppercased()
+        case reduseThickness: textView.text = textView.text.lowercased()
+        default:
+            return
+        }
+    }
+    
+    @IBAction func fontSizeSliderAction(_ sender: Any) {
+        textView.font = UIFont.systemFont(ofSize: CGFloat(fontSizeSlider.value))
+        
+    }
+    
+    @IBAction func changeColorAction(_ sender: UIButton) {
+        changeColor(button: sender)
+    }
+    
+    @IBAction func changeFontThickness(_ sender: UIButton) {
+        
+    }
+    
+    
+    
 
 
 }
